d4158ca89da46d088807342e8cc7ff6d700bf729
Pasi Hyvönen <Pasi Hyvnen>
2016-10-17 22:50:29 +0300

    Add project boilerplate

---
+++ src/components/mind-map.js
@@ -0,0 +1,9 @@
+import React, { createClass } from "react"
+
+const MindMap = createClass({
+    render: function() {
+        return <div>testing...</div>
+    }
+})
+
+export default MindMap
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { render } from "react-dom"
+
+import MindMap from "./components/mind-map"
+
+render(<MindMap/>, document.getElementById("app-root"))
