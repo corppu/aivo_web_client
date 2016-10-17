86e430cd47784a567a5cab9e6ca35fde8d36bf51
Pasi Hyvönen <Pasi Hyvnen>
2016-10-17 23:37:14 +0300

    Add naive canvas rendering component

--- src/components/mind-map.js
+++ src/components/mind-map.js
@@ -1,9 +1,48 @@
 import React, { createClass } from "react"
 
 const MindMap = createClass({
-    render: function() {
-        return <div>testing...</div>
+    componentDidMount: function() {
+        const ctx = this.refs.canvas.getContext("2d")
+
+        this.__running = true
+
+        const renderLoop = () => {
+            if (!this.__running) {
+                return
+            }
+            const props = this.props
+
+            renderMindMap(ctx, props)
+            requestAnimationFrame(renderLoop)    
+        }
+        requestAnimationFrame(renderLoop)   
+    },
+
+    componentWillUnmount: function() {
+        this.__running = false
+    },
+    
+    render: function() {    
+        return (
+            <div
+                style={{
+                    width: "100vw",
+                    height: "100vh"
+                }}>
+
+                <canvas
+                    ref="canvas"
+                    width={1000}
+                    height={1000}/>
+            </div>
+        )
     }
 })
+
+function renderMindMap(ctx, props) {
+    props.nodes.forEach((node) => {
+        ctx.fillRect(node.x, node.y, 10, 10)
+    })
+}
 
 export default MindMap
--- src/main.js
+++ src/main.js
@@ -3,4 +3,19 @@ import { render } from "react-dom"
 
 import MindMap from "./components/mind-map"
 
-render(<MindMap/>, document.getElementById("app-root"))
+const mockProps = {
+    nodes: [
+        {
+            title: "juttu",
+            x: 100,
+            y: 100
+        },
+        {
+            title: "huttu",
+            x: 200,
+            y: 400
+        }
+    ]
+}
+
+render(<MindMap {...mockProps}/>, document.getElementById("app-root"))
