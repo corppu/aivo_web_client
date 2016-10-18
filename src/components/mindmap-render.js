b2a0b2f88f882e26ddd49599e40f6d49b33dd716
Pasi Hyvönen <Pasi Hyvnen>
2016-10-18 22:15:38 +0300

    Added canvas clearing

--- src/components/mindmap-render.js
+++ src/components/mindmap-render.js
@@ -1,6 +1,8 @@
-import { drawCircle } from "../utils/canvas-utils"
+import { clear, drawCircle } from "../utils/canvas-utils"
 
 export default function(ctx, state) {
+    clear(ctx)
+
     state.nodes.forEach((node) => {
        drawCircle(ctx, {x: node.x, y: node.y, r: 20})
     })
--- src/utils/canvas-utils.js
+++ src/utils/canvas-utils.js
@@ -1,12 +1,20 @@
 
-export function drawRect(ctx, {x = 0, y = 0, w = 10, h = 10}) {
+export function clear(ctx, {color = "#FFF"} = {}) {
+    ctx.fillStyle = color
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+}
+
+export function drawRect(ctx, {x = 0, y = 0, w = 10, h = 10, color = "#000"} = {}) {
+    ctx.fillStyle = color
     ctx.fillRect(x - w/2, y - h/2, w, h)
 }
 
-export function drawCircle(ctx, {x = 0, y = 0, r = 5}) {
+export function drawCircle(ctx, {x = 0, y = 0, r = 5, color = "#000"} = {}) {
     ctx.beginPath()
     ctx.arc(x, y, r, 0, 2 * Math.PI)
 
+    ctx.fillStyle = color
     ctx.fill()
+    ctx.strokeStyle = color
     ctx.stroke()
 }
