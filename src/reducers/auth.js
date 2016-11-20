bfd49aa6e5364cd82e75c2e9d075451cb5022e4c
Pasi Hyvönen <Pasi Hyvnen>
2016-11-20 16:08:53 +0200

    Implement auth reducer

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,12 +1,23 @@
 import { fromJS } from "immutable"
 
+import {
+    BACKEND_LOGIN,
+    BACKEND_LOGOUT,
+} from "../constants/action-types"
+
 const initialState = fromJS({
     authed: false
 })
 
 export default function(state = initialState, action) {
     switch (action.type) {
-        default:
-            return state
+    case BACKEND_LOGIN:
+        return state.set("authed", true)
+
+    case BACKEND_LOGOUT:
+        return state.set("authed", false)
+    
+    default:
+        return state
     }
 }
