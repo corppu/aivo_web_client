e8550fd1a193f070c900df081d85890fa6cd9442
Pasi Hyvönen <Pasi Hyvnen>
2016-11-20 16:24:42 +0200

    Add login page base

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,15 @@
 import React, { createClass } from "react"
 
+import Login from "./login"
+
 const App = createClass({
     render: function() {
         const { authed, children } = this.props;
         
-        //console.log(authed)
-
         return (
             <div>
                 <h1>Aivo v0.0.1</h1>
-                {children}
+                {authed ? children : <Login/>}
             </div>
         )
     }
