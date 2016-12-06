20d02b76075912af685a5438ca3cbaaaba294417
Pasi Hyvönen <Pasi Hyvnen>
2016-12-06 15:37:13 +0200

    Add node view stump

---
+++ src/components/node-view.js
@@ -0,0 +1,13 @@
+import React, { createClass } from "react";
+
+const NodeView = createClass({
+    render: function() {
+        return (
+            <div>
+                TESTING!
+            </div>
+        );
+    }
+});
+
+export default NodeView;
---
+++ src/containers/node-view.js
@@ -0,0 +1,9 @@
+import { connect } from "react-redux";
+
+import NodeView from "../components/node-view";
+
+function mapStateToProps() {
+    return {};
+}
+
+export default connect(mapStateToProps)(NodeView);
