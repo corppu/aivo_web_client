6a54ef303a9204d5e9e8ecc01d04d08dd022d250
Pasi Hyvönen <Pasi Hyvnen>
2016-12-06 18:37:23 +0200

    Add board-list stump

---
+++ src/components/board-list.js
@@ -0,0 +1,13 @@
+import React, { createClass } from "react"
+
+const BoardList = createClass({
+    render: function() {
+        return (
+            <div>
+                board list!
+            </div>
+        );
+    }
+});
+
+export default BoardList;
---
+++ src/containers/board-list.js
@@ -0,0 +1,9 @@
+import { connect } from "react-redux";
+
+import BoardList from "../components/board-list";
+
+function mapStateToProps() {
+    return {};
+}
+
+export default connect(mapStateToProps)(BoardList);
