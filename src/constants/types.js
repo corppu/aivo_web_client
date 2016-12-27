ed7bd8e8a96c02861d54d71127b784adc4705c38
corppu <corppu91@gmail.com>
2016-12-27 14:22:15 +0200

    type constants/types.js

--- src/constants/types.js
+++ src/constants/types.js
@@ -2,3 +2,6 @@
 export const NODE_TYPE_UNDEFINED = "undefined";
 export const NODE_TYPE_TEXT = "text";
 export const NODE_TYPE_IMAGE = "image";
+export const TYPE_NODE = "node";
+export const TYPE_LINE = "line";
+export const TYPE_NONE = "none";
