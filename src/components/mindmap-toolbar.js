6d417d85235e98c9231a9957ae936ee17c42aa1f
Pasi Hyvönen <Pasi Hyvnen>
2016-12-28 19:20:29 +0200

    Add mindmap toolbar stump

---
+++ src/components/mindmap-toolbar.js
@@ -0,0 +1,26 @@
+import React, { createClass } from "react";
+
+const MindMapToolbar = createClass({
+    render: function() {
+        return (
+            <span>
+                { this.renderSearchBar() }
+            </span>
+        );
+    },
+
+    renderSearchBar: function() {
+        return (
+            <div
+                style={{
+                    position: "fixed",
+                    top: 0,
+                    left: 100
+                }}>
+                ...
+            </div>
+        );
+    }
+});
+
+export default MindMapToolbar;
