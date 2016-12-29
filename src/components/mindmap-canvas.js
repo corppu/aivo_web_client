9a4e78ca21e5e4f3bb94a5b49427d2e6fae07c0d
Pasi Hyvönen <Pasi Hyvnen>
2016-12-29 13:25:46 +0200

    Remove hammerJS

--- src/components/mindmap-canvas.js
+++ src/components/mindmap-canvas.js
@@ -14,11 +14,10 @@ import {
 } from "../constants/values";
 
 import { clear, createRenderer, transformToCamera } from "../utils/canvas-utils";
-//import { createAction, updateAction, actionResult } from "../utils/input-utils";
+import { createAction, updateAction, actionResult } from "../utils/input-utils";
 import { flagHidden } from "../utils/node-utils";
 
 export default function() {
-
 	let _engine = Engine.create();
     let _nodes = new Map();
 	let _lines = [];
@@ -192,41 +191,36 @@ export default function() {
             _nodes = flagHidden(_nodes, _searchFilter);
         }
     }
-
-	function moveCameraBy(dx, dy) {
-		_camera.x += dx;
-		_camera.y += dy;
-	}
 	
-	function getCamera() {
-		return _camera;
-	}
-	
-    /*
     function onInputStart(e) {
         const pos = transformToCamera(_camera, e.position);
         _inputAction = createAction(pos);
     }
 
     function onInputEnd(e) {
-        // // if (!_inputAction) {
-            // // return;
-        // // }
+        if (!_inputAction) {
+            return;
+        }
         const pos = transformToCamera(_camera, e.position);
 
-        // updateAction(_inputAction, pos);
-        // const result = actionResult(_inputAction, pos);
+        updateAction(_inputAction, pos);
+        const result = actionResult(_inputAction, pos);
 	        
         const hits = Query.point(_engine.world.bodies, pos);
         if (hits.length > 0) {
-            //hits.forEach(body => {
-                //const node = _bodyToNodeMapping[body.id];
+            
+            // delete node
+            /*
+            hits.forEach(body => {
+                const node = _bodyToNodeMapping[body.id];
 
-                //if (_actions.removeNode) {
-                    //_actions.removeNode(node.id)
-                //}
-            //})
+                if (_actions.removeNode) {
+                    _actions.removeNode(node.id)
+                }
+            })
+            */
 
+            // move node
             if (_inputAction.totalDeltaMagnitude <= 10) {
                 const node = _bodyToNodeMapping[hits[0].id];
                 
@@ -248,16 +242,18 @@ export default function() {
                 }
             }
         }
-
         _inputAction = null
     }
 
     function onInputMove(e) {
+        if (!_inputAction) {
+            return;
+        }
         const pos = transformToCamera(_camera, e.position);
         updateAction(_inputAction, pos);
+
         Object.assign(_camera, Vector.add(_camera, _inputAction.lastDelta));
     }
-    */
 
     function update() {
         updateFps(); // Just testing...
@@ -299,8 +295,9 @@ export default function() {
 
     return {
         updateProps,
-		getCamera,
-		moveCameraBy,
+        onInputStart,
+        onInputEnd,
+        onInputMove,
         update,
         render
     };
--- src/components/mindmap.js
+++ src/components/mindmap.js
@@ -1,8 +1,6 @@
-// Presenter for the Mindmap
 import React, { createClass } from "react";
-import createMindmap from "./mindmap-canvas";
-import Hammer from "react-hammerjs";
 
+import createMindmap from "./mindmap-canvas";
 import MindMapToolbar from "./mindmap-toolbar";
 
 import {
@@ -18,11 +16,6 @@ import {
 	NODE_TXT_BOX_WIDTH,
 	NODE_TXT_BOX_HEIGHT
 } from "../constants/values";
-
-let _pressed = false;
-let _pressedObj = null;
-let _selectedObj = null;
-let _lastPoint = {x: 0, y: 0};
 
 const MindMap = createClass({
     getInitialState: function() {
@@ -81,101 +74,90 @@ const MindMap = createClass({
         return (
 			<div>
 				<MindMapToolbar/>
-				<Hammer onTap={this.handleTap} onDoubleTap={this.handleDoubleTap} onPress={this.handlePress}>
-					<div
-						style={{
-							position: "fixed",
-							top: 0,
-							left: 0,
-							width: "100vw",
-							height: "100vh",
-							overflow: "hidden",
-							cursor: "default",
-							zIndex: -1
-						}}>
+				<div
+					style={{
+						position: "fixed",
+						top: 0,
+						left: 0,
+						width: "100vw",
+						height: "100vh",
+						overflow: "hidden",
+						cursor: "default",
+						zIndex: -1
+					}}>
 
-						<canvas
-							ref={(canvas) => { this.canvas = canvas; }}
-							width={width}
-							height={height}
-							onTouchStart={this.handleTouchStart}
-							onMouseDown={this.handleMouseDown}
-							onTouchMove={this.handleTouchMove} // MOVE
-							onMouseMove={this.handleMouseMove} // MOVE
-							onMouseUp={this.handleMouseEnd} // END
-							onMouseLeave={this.handleMouseEnd} // END
-							onTouchEnd={this.handleTouchEnd} // END
-							onTouchCancel={this.handleTouchEnd} // END
-							/>
-						{children}
-					</div>
-				</Hammer>
+					<canvas
+						ref={(canvas) => { this.canvas = canvas; }}
+						width={width}
+						height={height}
+
+						onTouchStart={this.handleTouchStart}
+						onTouchMove={this.handleTouchMove}
+						onTouchEnd={this.handleTouchEnd}
+						onTouchCancel={this.handleTouchEnd}
+						onMouseDown={this.handleMouseStart}
+						onMouseUp={this.handleMouseEnd}
+						onMouseLeave={this.handleMouseEnd}
+						onMouseMove={this.handleMouseMove}
+						/>
+					{children}
+				</div>
 			</div>
         )
     },
 
 	handleTouchStart: function(e) {
-		if(e.touches.length > 1)
-			return;
-
-		_lastPoint = calculatePoint(this.canvas, this.mindmap.getCamera(), e.changedTouches[0].clientX, e.changedTouches[0].clientY);
-		_pressed = true;
+		const { clientX, clientY } = e.changedTouches[0];
+        if (this.mindmap) {
+            this.mindmap.onInputStart({
+                position: calculatePosition(this.canvas, clientX, clientY)
+            });
+        }		
 	},
 	
-	handleMouseDown: function(e) {
-		_lastPoint = calculatePoint(this.canvas, this.mindmap.getCamera(), e.clientX, e.clientY);
-		_pressed = true;
-	},
-	
-	handleMoveNode: function(point) {
-		const value = this.props.nodes.get(_pressedObj.id);
-		this.props.tryUpdateNode(
-			_pressedObj.id, 
-			{
-				type: value.get("type") || NODE_TYPE_UNDEFINED,
-				x: point.x,
-				y: point.y,
-				title: value.get("title"),
-				text: value.get("text") || null,
-				imgURL: value.get("imgURL") || null	
-			}
-		);
-	},
-	
-	handleMoveCamera: function(point) {
-		this.mindmap.moveCameraBy(_lastPoint.x - point.x, _lastPoint.y - point.y);
-		_lastPoint = point;
+	handleTouchEnd: function(e) {
+		const { clientX, clientY } = e.changedTouches[0];
+        if (this.mindmap) {
+            this.mindmap.onInputEnd({
+                position: calculatePosition(this.canvas, clientX, clientY)
+            });
+        }
 	},
 	
 	handleTouchMove: function(e) {
-		const point = calculatePoint(this.canvas, this.mindmap.getCamera(), e.changedTouches[0].clientX, e.changedTouches[0].clientY);
-
-		if(e.touches.length === 1 && _pressedObj) {
-			
-			if(_pressedObj.type === TYPE_NODE) {
-				this.handleMoveNode(point);
-			}
-			
-		} 
-		
-		else if(e.touches.length === 1 && !_pressedObj) {
-			this.handleMoveCamera(point);
-		}
+		const { clientX, clientY } = e.changedTouches[0];
+        if (this.mindmap) {
+            this.mindmap.onInputMove({
+                position: calculatePosition(this.canvas, clientX, clientY)
+            });
+        }	
 	},
 	
-    handleMouseMove: function(e) {
-		if(_pressed) {
-			const point = calculatePoint(this.canvas, this.mindmap.getCamera(), e.clientX, e.clientY);
+    handleMouseStart: function(e) {
+		const { clientX, clientY } = e;
+        if (this.mindmap) {
+            this.mindmap.onInputStart({
+                position: calculatePosition(this.canvas, clientX, clientY)
+            });
+        }
+    },
 
-			if(_pressedObj) {
-				if(_pressedObj.type === TYPE_NODE) {
-					this.handleMoveNode(point);
-				}
-			}
-			else {
-				this.handleMoveCamera(point);
-			}
-		}
+    handleMouseEnd: function(e) {
+		const { clientX, clientY } = e;
+        if (this.mindmap) {
+            this.mindmap.onInputEnd({
+                position: calculatePosition(this.canvas, clientX, clientY)
+            });
+        }
+    },
+
+    handleMouseMove: function(e) {
+		const { clientX, clientY } = e;
+        if (this.mindmap) {
+            this.mindmap.onInputMove({
+                position: calculatePosition(this.canvas, clientX, clientY)
+            });
+        }
     },
 
     handleResize: function() {
@@ -185,115 +167,16 @@ const MindMap = createClass({
             width: innerWidth,
             height: innerHeight
         });
-    },
-	
-	handleTouchEnd: function(e) {
-		_pressedObj = null;
-		_pressed = false;
-	},
-
-	handleMouseEnd: function(e) {
-		_pressedObj = null;
-		_pressed = false;
-    },
-	/*
-	    _actions.addNode = props.tryAddNode(data);
-		_actions.addLine = props.tryAddLine(data);
-        _actions.updateNode = props.tryUpdateNode(id, data);
-		_actions.updateLine = props.tryUpdateLine(id,data);
-        _actions.removeNode = props.tryRemoveNode(id);
-		_actions.removeLine = props.tryRemoveLine(id);
-        _actions.openNode = props.openNode(id);
-	*/
-	
-	handleTap: function(e) {
-		console.log("TAP on (" + e.center.x.toString() + "," + e.center.y.toString() + ")");
-		
-		const point = calculatePoint(this.canvas, this.mindmap.getCamera(), e.center.x, e.center.y);
-		
-		_selectedObj = trySelectNode(point, this.props.nodes);
-	},
-
-	handleDoubleTap: function(e) {
-		console.log("DOUBLE TAP on (" + e.center.x.toString() + "," + e.center.y.toString() + ")");
-		
-		const point = calculatePoint(this.canvas, this.mindmap.getCamera(), e.center.x, e.center.y);
-		
-		if(_selectedObj && _selectedObj.type === TYPE_NODE) {
-			const value = this.props.nodes.get(_selectedObj.id);
-			if(!value) {
-				_selectedObj = null;
-				return;
-			}
-			if(pointInCircle(point.x, point.y, value.get("x"), value.get("y"), NODE_RADIUS)) {
-				this.props.openNode(_selectedObj.id);
-			}
-		}
-
-		else if(!_selectedObj) {
-			this.props.tryAddNode({
-				title: "parent example",
-				type: NODE_TYPE_UNDEFINED,
-				imgURL: "http://xpenology.org/wp-content/themes/qaengine/img/default-thumbnail.jpg",
-				x: point.x,
-				y: point.y
-			});
-		}
-	},
-
-	handlePress: function(e) {
-		console.log("PRESS on (" + e.center.x.toString() + "," + e.center.y.toString() + ")");
-		const point = calculatePoint(this.canvas, this.mindmap.getCamera(), e.center.x, e.center.y);
-		_selectedObj = trySelectNode(point, this.props.nodes);
-		_pressedObj = _selectedObj;
-		
-		if(_pressedObj) {
-			console.log("Pressed object: " + _pressedObj.type + " " + _pressedObj.id);
-		}
-	}
+    }
 });
 
-function calculatePoint(canvas, camera, clientX, clientY) {
+function calculatePosition(canvas, clientX, clientY) {
     const bounds = canvas.getBoundingClientRect();
 
     return {
-        x: (clientX - bounds.left) + camera.x,
-        y: (clientY - bounds.top) + camera.y
+        x: clientX - bounds.left,
+        y: clientY - bounds.top
     };
-}
-
-function trySelectNode(point, nodes) {
-	let rVal = null;
-	
-	for (var [key, value] of nodes) {
-		if(pointInCircle(point.x, point.y, value.get("x"), value.get("y"), NODE_RADIUS)) {
-			rVal = {
-				type: TYPE_NODE,
-				id: key,
-			}
-			
-			console.log("Node selected: " + key);
-			break;
-		}
-	}
-	
-	return rVal;
-}
-
-// x,y is the point to test
-// cx, cy is circle center, and radius is circle radius
-function pointInCircle(x, y, cx, cy, radius) {
-	var distancesquared = (x - cx) * (x - cx) + (y - cy) * (y - cy);
-	return distancesquared <= radius * radius;
-}
-
-function pointInRect(point, rectX, rectY, rectW, rectH) {
-	const left =  rectX - rectW / 2;
-	const right = rectX + rectW / 2;
-	const top = rectY - rectH / 2;
-	const btm = rectY + rectH / 2;
-	
-	return (left <= point.x && point.x <= right && top <= point.y && point.y <= btm);
 }
 
 export default MindMap;
