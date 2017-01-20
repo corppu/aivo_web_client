63542585dea76732875418702a4476dd3e67891d
corppu <corppu91@gmail.com>
2017-01-20 19:18:39 +0200

    nothing functional... just code styling

--- src/components/mindmap-canvas.js
+++ src/components/mindmap-canvas.js
@@ -81,113 +81,113 @@ export default function() {
 		let propsPins = new Map( props.pins );
 		
         // match existing nodes to props (update old ones)
-        _context.nodes.forEach((node, id) => {
-            const propsNode = propsNodes.get(id);
+        _context.nodes.forEach( ( node, id ) => {
+            const propsNode = propsNodes.get( id );
 
-             if (!propsNode) {
+             if ( !propsNode ) {
                  const { body } = node;
 
-                _context.nodes.delete(id)
-                _context.bodyToNodeMapping.delete(body.id);
-                World.remove(_context.engine.world, body);
+                _context.nodes.delete( id )
+                _context.bodyToNodeMapping.delete( body.id );
+                World.remove( _context.engine.world, body );
 
-				if(_selectedNode && _selectedNode.id === id) {
+				if( _selectedNode && _selectedNode.id === id ) {
 					_selectedNode = null;
 				}
 				
                 //console.log(`removed node ${id}`);
                 return;
             }
-            propsNodes.delete(id);
+            propsNodes.delete( id );
 			
-            Object.assign(node, {
+            Object.assign( node, {
 				id,
-				primaryType: propsNode.get("primaryType"),
-                type: propsNode.get("type"),
+				primaryType: propsNode.get( "primaryType" ),
+                type: propsNode.get( "type" ),
                 anchor: {
-                    x: propsNode.get("x"),
-                    y: propsNode.get("y")
+                    x: propsNode.get( "x" ),
+                    y: propsNode.get( "y" )
                 },
-				x : propsNode.get("x"),
-				y : propsNode.get("y"),
-                title: propsNode.get("title") || "",
-                text: propsNode.get("text") || "",
-                imgURL: propsNode.get("imgURL") || "",
+				x : propsNode.get( "x" ),
+				y : propsNode.get( "y" ),
+                title: propsNode.get( "title" ) || "",
+                text: propsNode.get( "text" ) || "",
+                imgURL: propsNode.get( "imgURL" ) || "",
             });
 			
-			var tempLines = propsNode.get("lines");
-			if(tempLines) {
+			var tempLines = propsNode.get( "lines" );
+			if( tempLines ) {
 				node.lines = tempLines.toObject();
 			}
 			//console.log("updated  -> " + node);
-        });
+        } );
 
 
 		
 		// match existing lines to props (update old ones)
-          _context.lines.forEach((line, id) => {
-            const propsLine = propsLines.get(id);
+          _context.lines.forEach( ( line, id ) => {
+            const propsLine = propsLines.get( id );
 
-             if (!propsLine) {
+             if ( !propsLine ) {
                 // const { body } = line;
 
-                _context.lines.delete(id)
+                _context.lines.delete( id )
                 // _context.bodyToNodeMapping.delete(body.id);
                 // World.remove(_context.engine.world, body);
 
                 //console.log(`removed line ${id}`);
                 return;
             }
-            propsLines.delete(id);
+            propsLines.delete( id );
 
 			//console.log("updated -> " + line);
 
-            return Object.assign(line, {
+            return Object.assign( line, {
 				id,
-				primaryType: propsLine.get("primaryType"),
-				parentType: propsLine.get("parentType"),
-				parentId: propsLine.get("parentId"),
-				childType: propsLine.get("childType"),
-				childId: propsLine.get("childId")
-            });
-        });		
+				primaryType: propsLine.get( "primaryType" ),
+				parentType: propsLine.get( "parentType" ),
+				parentId: propsLine.get( "parentId" ),
+				childType: propsLine.get( "childType" ),
+				childId: propsLine.get( "childId" )
+            } );
+        } );		
 		
 		
 		
 		
 		// match existing pins to props (update old ones)
-        _context.pins.forEach((pin, id) => {
-            const propsPin = propsPins.get(id);
+        _context.pins.forEach( ( pin, id ) => {
+            const propsPin = propsPins.get( id );
 
-             if (!propsPin) {
+             if ( !propsPin ) {
                  const { body } = pin;
 
-                _context.pins.delete(id)
+                _context.pins.delete( id )
                 //_context.bodyToNodeMapping.delete(body.id);
                 //World.remove(_context.engine.world, body);
 
                 //console.log(`removed pin ${id}`);
                 return;
             }
-            propsPins.delete(id);
+            propsPins.delete( id );
 			//		lines: {lineIdToPin: parentId}
 
-            Object.assign(pin, {
+            Object.assign( pin, {
 				id,
-				primaryType: propsPin.get("primaryType"),
+				primaryType: propsPin.get( "primaryType" ),
 				anchor: {
-                    x: propsPin.get("x"),
-                    y: propsPin.get("y")
+                    x: propsPin.get( "x" ),
+                    y: propsPin.get( "y" )
                 },
-				x: propsPin.get("x"),
-				y: propsPin.get("y")
+				x: propsPin.get( "x" ),
+				y: propsPin.get( "y" )
             });
-			var tempLines = propsPin.get("lines");
-			if(tempLines) {
+			var tempLines = propsPin.get( "lines" );
+			if( tempLines ) {
 				pin.lines = tempLines.toObject();
 			}
 			//console.log(pin);
-        });
+        } );
 2		
 		
 		
@@ -197,23 +197,23 @@ export default function() {
         //_context.lines = _context.lines.filter(line => line !== null);
 
         // add new nodes (were in props and not in state)
-        propsNodes.forEach((propsNode, id) => {
+        propsNodes.forEach( ( propsNode, id ) => {
             const radius = MINDMAP_NODE_RADIUS;
             const anchor = {
-                x: propsNode.get("x"),
-                y: propsNode.get("y")
+                x: propsNode.get( "x" ),
+                y: propsNode.get( "y" )
             };
-            const body = Bodies.circle(anchor.x, anchor.y, radius, {
+            const body = Bodies.circle( anchor.x, anchor.y, radius, {
                 frictionAir: 1,
                 mass: 5
-            });
+            } );
             const node = {
 				id,
-				primaryType : propsNode.get("primaryType"),
-                type: propsNode.get("type"),
-                title: propsNode.get("title") || "",
-                text: propsNode.get("text") || "",
-				imgURL: propsNode.get("imgURL") || "",
+				primaryType : propsNode.get( "primaryType" ),
+                type: propsNode.get( "type" ),
+                title: propsNode.get( "title" ) || "",
+                text: propsNode.get( "text" ) || "",
+				imgURL: propsNode.get( "imgURL" ) || "",
                 radius,
 				x: anchor.x,
 				y: anchor.y,
@@ -221,51 +221,51 @@ export default function() {
                 body
             };
 			
-			var tempLines = propsNode.get("lines");
-			if(tempLines) {
+			var tempLines = propsNode.get( "lines" );
+			if( tempLines ) {
 				node.lines = tempLines.toObject();
 			}
 			
 			//console.log(node);
 			
-            _context.nodes.set(id, node);
-            _context.bodyToNodeMapping[body.id] = node;
-            World.add(_context.engine.world, body);
+            _context.nodes.set( id, node );
+            _context.bodyToNodeMapping[ body.id ] = node;
+            World.add( _context.engine.world, body );
 
             //console.log(`added node ${id}`);
-        })
+        } )
 		
 		 // add new Lines (were in props and not in state)
-        propsLines.forEach((propsLine, id) => {
+        propsLines.forEach( ( propsLine, id ) => {
 			const line = {
 				id,
-				primaryType: propsLine.get("primaryType"),
-				parentType: propsLine.get("parentType"),
-				parentId: propsLine.get("parentId"),
-				childType: propsLine.get("childType"),
-				childId: propsLine.get("childId")
+				primaryType: propsLine.get( "primaryType" ),
+				parentType: propsLine.get( "parentType" ),
+				parentId: propsLine.get( "parentId" ),
+				childType: propsLine.get( "childType" ),
+				childId: propsLine.get( "childId" )
             };
 			//console.log(line);
-		   _context.lines.set(id, line);
+		   _context.lines.set( id, line );
 			//console.log(`added line ${id}`);
-		});
+		} );
 
 		
 		
 		// add new pins (were in props and not in state)
-        propsPins.forEach((propsPin, id) => {
+        propsPins.forEach( ( propsPin, id ) => {
             const radius = MINDMAP_PIN_RADIUS;
             const anchor = {
                 x: propsPin.get("x"),
                 y: propsPin.get("y")
             };
-            const body = Bodies.circle(anchor.x, anchor.y, radius, {
+            const body = Bodies.circle( anchor.x, anchor.y, radius, {
                 frictionAir: 1,
                 mass: 5
-            });
+            } );
             const pin = {
 				id,
-				primaryType: propsPin.get("primaryType"),
+				primaryType: propsPin( "primaryType" ),
 				x: anchor.x,
 				y: anchor.y,
                 radius,
@@ -273,41 +273,41 @@ export default function() {
                 body
             };
 			
-			var tempLines = propsPin.get("lines");
-			if(tempLines) {
+			var tempLines = propsPin.get( "lines" );
+			if( tempLines ) {
 				pin.lines = tempLines.toObject();
 			}
-            _context.pins.set(id, pin);
+            _context.pins.set( id, pin );
             //_context.bodyToNodeMapping[body.id] = node;
             //World.add(_context.engine.world, body);
 
             //console.log(`added pin ${id}`);
-        })
+        } )
 		
 		
         // search filtering
-        if (props.searchFilter && props.searchFilter !== _searchFilter) {
+        if ( props.searchFilter && props.searchFilter !== _searchFilter ) {
             _searchFilter = props.searchFilter;
 
-            _context.nodes = flagHidden(_context.nodes, _searchFilter);
+            _context.nodes = flagHidden( _context.nodes, _searchFilter );
         }
     }
 	
-    function onInputStart(action) {
-        const pos = translateToCamera(_camera, action.startPosition);
+    function onInputStart( action ) {
+        const pos = translateToCamera( _camera, action.startPosition );
 
-        return queryNodeAtPoint(_context, pos);
+        return queryNodeAtPoint( _context, pos );
     }
 
-    function onInputEnd(action) {
-        const pos = translateToCamera(_camera, action.endPosition);
+    function onInputEnd( action ) {
+        const pos = translateToCamera( _camera, action.endPosition );
 	    
-        const hits = Query.point(_context.engine.world.bodies, pos);
+        const hits = Query.point( _context.engine.world.bodies, pos );
         
-		if (hits.length > 0) {
-             const node = _context.bodyToNodeMapping[hits[0].id];
-             if (action.totalDeltaMagnitude <= 10) {
-                 if (_selectedNode === null || _selectedNode.id !== node.id) {
+		if ( hits.length > 0 ) {
+             const node = _context.bodyToNodeMapping[ hits[ 0 ].id ];
+             if ( action.totalDeltaMagnitude <= 10 ) {
+                 if ( _selectedNode === null || _selectedNode.id !== node.id ) {
                      _selectedNode = node;
                  }
 				 else {
@@ -317,7 +317,7 @@ export default function() {
 				 }
 			 }
 		}
-		else if (action.totalDeltaMagnitude <= 10) {
+		else if ( action.totalDeltaMagnitude <= 10 ) {
 			 _selectedNode = null;
 			 action.data = null;
 		}
@@ -326,26 +326,26 @@ export default function() {
 	}
 	
 
-    function onInputMove(action) {
-        const pos = translateToCamera(_camera, action.endPosition);
+    function onInputMove( action ) {
+        const pos = translateToCamera( _camera, action.endPosition );
 
-        if (action.data) {
-            if (_actions.updateObject) {
+        if ( action.data ) {
+            if ( _actions.updateObject ) {
 				action.data.x = pos.x;
 				action.data.y = pos.y;
 				//console.log(action.data);
-			   _actions.updateObject(action.data);
+			   _actions.updateObject( action.data );
 			}
          }
 		 else {
-             Object.assign(_camera, Vector.add(_camera, action.lastDelta));
+             Object.assign( _camera, Vector.add( _camera, action.lastDelta ) );
          }
     }
 
-    function onLongPress(action) {
+    function onLongPress( action ) {
         //console.log(action);
-		 const pos = translateToCamera(_camera, action.endPosition);
-		 if(action.data) {
+		 const pos = translateToCamera( _camera, action.endPosition );
+		 if( action.data ) {
 			 _actions.createObject( 
 				{
 					primaryType: TYPE_NODE,
@@ -356,59 +356,59 @@ export default function() {
 				action.data
 			);
 		 }
-		else _actions.createObject({primaryType: TYPE_NODE, x: pos.x, y: pos.y});
+		else _actions.createObject( { primaryType: TYPE_NODE, x: pos.x, y: pos.y } );
     }
 
     function update() {
         updateFps(); // Just testing...
 
-        _context.nodes.forEach((node) => {
+        _context.nodes.forEach( ( node ) => {
             const { radius, anchor, body } = node;
 
-            const diff = Vector.sub(anchor, body.position);
+            const diff = Vector.sub( anchor, body.position );
 
             //if (Vector.magnitude(diff) > radius * 1.5) {
-                const vel = Vector.mult(diff, 1/1000);
+                const vel = Vector.mult( diff, 1/1000 );
 
-                Body.applyForce(body, Vector.create(0, 0), vel);
+                Body.applyForce( body, Vector.create( 0, 0 ), vel );
             //}
-        });
+        } );
 
         // update physics
         _context.engine.world.gravity.x = 0;
         _context.engine.world.gravity.y = 0;
         
-        Engine.update(_context.engine);
+        Engine.update( _context.engine );
     }
     
-    function render(ctx) {
-		clear(ctx, { color: "#f0f0f0" });
+    function render( ctx ) {
+		clear( ctx, { color: "#f0f0f0" } );
 
-        const draw = createRenderer(ctx, { camera: _camera });
+        const draw = createRenderer( ctx, { camera: _camera } );
 
-		_context.lines.forEach(line => {
+		_context.lines.forEach( line => {
             drawLine(
 				draw,
 				line,
 				_context.engine.world.bodies,
-				line.parentType === "node" ? _context.nodes.get(line.parentId) : _context.pins.get(line.parentId), 
-				line.childType === "node" ? _context.nodes.get(line.childId) : _context.pins.get(line.childId)
+				line.parentType === "node" ? _context.nodes.get( line.parentId ) : _context.pins.get( line.parentId ), 
+				line.childType === "node" ? _context.nodes.get( line.childId ) : _context.pins.get( line.childId )
 			);
-        });
+        } );
 		
         // draw non-selected node(s)
-        _context.nodes.forEach(node => {
-            if (node !== _selectedNode) {
-                drawNode(draw, node, false);
+        _context.nodes.forEach( node => {
+            if ( node !== _selectedNode ) {
+                drawNode( draw, node, false );
             }
-        });
+        } );
 
         // draw selected node(s)
-        if (_selectedNode !== null) {
-            drawNode(draw, _selectedNode, true);
+        if ( _selectedNode !== null ) {
+            drawNode( draw, _selectedNode, true );
         }
 
-		drawFPS(draw, _fps);
+		drawFPS( draw, _fps );
     }
 
     return {
@@ -422,91 +422,57 @@ export default function() {
     };
 }
 
-function drawFPS(draw, fps) {
-    draw.text({
+function drawFPS( draw, fps ) {
+    draw.text( {
         text: fps,
         x: 5,
         y: 5,
         baseline: "hanging",
         ignoreCamera: true
-    });
+    } );
 }
 
-function findAnchors(parentAnchor, childAnchor) {
-	let anchors = {
-		parentAnchor: {
-			x: parentAnchor.x,
-			y: parentAnchor.y
-		},
-		
-		childAnchor: {
-			x: childAnchor.x,
-			y: childAnchor.y
-		}
-	};
-	
-	if (parentAnchor.x < childAnchor.x) {
-		anchors.parentAnchor.x = parentAnchor.x + MINDMAP_NODE_RADIUS;
-		anchors.childAnchor.x = childAnchor.x - MINDMAP_NODE_RADIUS;
-	}
-	else if (parentAnchor.x > childAnchor.x) {
-		anchors.parentAnchor.x = parentAnchor.x - MINDMAP_NODE_RADIUS;
-		anchors.childAnchor.x = childAnchor.y + MINDMAP_NODE_RADIUS;
-	}
-	
-	if (parentAnchor.y < childAnchor.y) {
-		anchors.parentAnchor.y = parentAnchor.y + MINDMAP_NODE_RADIUS;
-		anchors.childAnchor.y = childAnchor.y - MINDMAP_NODE_RADIUS;
-	}
-	else if (parentAnchor.y > childAnchor.y) {
-		anchors.parentAnchor.y = parentAnchor.y - MINDMAP_NODE_RADIUS;
-		anchors.childAnchor.y = childAnchor.y + MINDMAP_NODE_RADIUS;
-	}
-	
-	return anchors;
-}
-
-function drawLine(draw, line, bodies, parentNode, childNode) {
+function drawLine( draw, line, bodies, parentNode, childNode ) {
 	//const anchors = findAnchors(parentNode.anchor, childNode.anchor);
-	if(!parentNode || !childNode) return;
+	if( !parentNode || !childNode ) return;
 	draw.curve(
-		findPath(bodies, parentNode.body, childNode.body)
+		findPath( bodies, parentNode.body, childNode.body )
 	);
 }
 
-function drawNode(draw, { type, imgURL, title, body, radius, hidden}, isSelected = false) {
-    if (hidden) {
+function drawNode( draw, { type, imgURL, title, body, radius, hidden }, isSelected = false ) {
+    if ( hidden ) {
         return;
     }
     const { x, y } = body.position;
 
     // draw selection highlight
-    if (isSelected) {
-        draw.circle({x, y, r: radius + MINDMAP_NODE_HIGHLIGHT_MARGIN, color: "red"});
+    if ( isSelected ) {
+        draw.circle( { x, y, r: radius + MINDMAP_NODE_HIGHLIGHT_MARGIN, color: "red" } );
     }
 
     // draw node graphic
-    switch (type) {
+    switch ( type ) {
     case NODE_TYPE_TEXT:
-        draw.circle({x, y, r: radius,
-                color: "#080", strokeColor: "black", strokeWidth: 2});
+        draw.circle( { x, y, r: radius,
+                color: "#080", strokeColor: "black", strokeWidth: 2 } );
         break;
     
     case NODE_TYPE_IMAGE:
-        draw.circle({x, y, r: radius,
-                color: "#088", imageURL: imgURL, strokeColor: "black", strokeWidth: 2});
+        draw.circle( { x, y, r: radius,
+                color: "#088", imageURL: imgURL, strokeColor: "black", strokeWidth: 2 } );
         break;
 
     default:
-        draw.circle({x, y, r: radius,
-                color: "#888", strokeColor: "black", strokeWidth: 2});
-        break; 
+        draw.circle( { x, y, r: radius,
+                color: "#888", strokeColor: "black", strokeWidth: 2 } );
+        break;
     }
 
     // draw the title
-    if (title.length > 10) {
-        title = title.substring(0, 7);
+    if ( title.length > 10 ) {
+        title = title.substring( 0, 7 );
 		title += "...";
     }
-    draw.text({text: title, x, y: y + radius * 2, baseline: "middle", align: "center"});
+    draw.text( { text: title, x, y: y + radius * 2, baseline: "middle", align: "center" } );
 }
