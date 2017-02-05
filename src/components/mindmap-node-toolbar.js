3477290d477f6591b714dfe5272bfcecdf121236
Pasi Hyvönen <Pasi Hyvnen>
2017-02-05 19:32:33 +0200

    Add submenu and some styling to node toolbar

--- src/components/mindmap-node-toolbar.js
+++ src/components/mindmap-node-toolbar.js
@@ -1,18 +1,66 @@
 import React, { createClass } from "react";
 
+const STATE_NONE = ""
+const STATE_TYPE_SELECT = "TYPE_SELECT"
+const STATE_STYLE_SELECT = "STYLE_SELECT"
+const STATE_COLOR_SELECT = "COLOR_SELECT"
+
 const MindMapNodeToolbar = createClass({
+    getInitialState: function() {
+        return {
+            subMenuState: STATE_NONE
+        }
+    },
+
     render: function() {
+        const { id } = this.props;
+
         return (
             <div
                 style={{
                     display: "flex",
                     position: "fixed",
-                    bottom: 0,
+                    bottom: 10,
                     width: "100%",
                     justifyContent: "center"
                 }}>
-                hello!
+                <div
+                    style={{
+                        padding: "5px 10px",
+                        backgroundColor: "#fff"
+                    }}>
+                    { id }
+                </div>
             </div>
+        );
+    },
+
+    renderSubMenu: function() {
+        const { subMenuState } = this.state;
+
+        switch (subMenuState) {
+            case STATE_TYPE_SELECT:     return this.renderTypeSelect;
+            case STATE_STYLE_SELECT:     return this.renderStyleSelect;
+            case STATE_COLOR_SELECT:     return this.renderColorSelect;
+        }
+        return null;
+    },
+
+    renderTypeSelect: function() {
+        return (
+            <div>type select</div>
+        );
+    },
+
+    renderStyleSelect: function() {
+        return (
+            <div>style select</div>
+        );
+    },
+
+    renderColorSelect: function() {
+        return (
+            <div>color select</div>
         );
     }
 });
