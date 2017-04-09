8ce609d0b09be7a4f97ad9d6aba85ff53daa097f
Pasi Hyvönen <Pasi Hyvnen>
2017-04-09 15:32:15 +0300

    Layout and logic tweaks for node toolbar

--- src/components/mindmap-node-toolbar.js
+++ src/components/mindmap-node-toolbar.js
@@ -16,19 +16,20 @@ const STATE_COLOR_SELECT = "COLOR_SELECT";
 const MindMapNodeToolbar = createClass({
     getInitialState: function() {
         return {
-            subMenuState: STATE_NONE
+            subMenuState: STATE_TYPE_SELECT
         }
     },
 
     render: function() {
-        const { id, primaryType, update, remove } = this.props;
+        const { id, data, update, remove } = this.props;
+        const type = data.type || "Type";
 
         return (
             <div
                 style={{
                     display: "flex",
                     position: "fixed",
-                    bottom: 10,
+                    bottom: 20,
                     width: "100%",
                     justifyContent: "center"
                 }}>
@@ -37,33 +38,27 @@ const MindMapNodeToolbar = createClass({
                         padding: "5px 10px",
                         backgroundColor: "#fff"
                     }}>
-                    { id }
-                    <Separator/>
-                    { primaryType }
-                    <Separator/>
                     <i
                         className="fa fa-circle-o fa-lg"
                         onClick={() => {
                             update({
-                                //primaryType: TYPE_CLUSTER
-                                x: 0
+                                // primaryType: TYPE_CLUSTER
                             });
                         }}/>
                     <Separator/>
                     <i
                         className="fa fa-circle fa-lg"
                         onClick={() => {
-                            /*
-                            update({
-                                customColor: "green"
-                            });
-                            */
+                            update({ customColor: "green" });
                         }}/>
+                    <Separator/>
+                    { type }
                     <Separator/>
                     <i
                         className="fa fa-trash-o fa-lg"
                         onClick={remove}/>
                 </div>
+                { this.renderSubMenu() }
             </div>
         );
     },
@@ -71,36 +66,57 @@ const MindMapNodeToolbar = createClass({
     renderSubMenu: function() {
         const { subMenuState } = this.state;
 
+        let child = null;
         switch (subMenuState) {
-            case STATE_TYPE_SELECT:     return this.renderTypeSelect;
-            case STATE_STYLE_SELECT:     return this.renderStyleSelect;
-            case STATE_COLOR_SELECT:     return this.renderColorSelect;
+            case STATE_TYPE_SELECT:     child = <TypeSelect/>; break;
+            case STATE_STYLE_SELECT:    child = <StyleSelect/>; break;
+            case STATE_COLOR_SELECT:    child = <ColorSelect/>; break;
         }
-        return null;
-    },
-
-    renderTypeSelect: function() {
+        if (!child) {
+            return null;
+        }
         return (
-            <div>type select</div>
-        );
-    },
-
-    renderStyleSelect: function() {
-        return (
-            <div>style select</div>
-        );
-    },
-
-    renderColorSelect: function() {
-        return (
-            <div>color select</div>
+            <div
+                style={{
+                    marginLeft: 10, // for temp layout
+                    backgroundColor: "#fff"
+                }}>
+                { child }
+            </div>
         );
     }
 });
 
+/*
+    fa-file-text-o
+    fa-file-image-o
+    fa-check-square-o
+    fa-share-alt
+*/
+
+function TypeSelect() {
+    return (
+        <div>
+            TYPE SELECT PLACEHOLDER
+        </div>
+    );
+}
+
+function StyleSelect() {
+     return (
+        <div>STYLE SELECT PLACEHOLDER</div>
+    );
+}
+
+function ColorSelect() {
+    return (
+        <div>COLOR SELECT PLACEHOLDER</div>
+    );
+}
+
 function Separator() {
     const style = {
-        color: "#bbb"
+        color: "#000"
     };
     return (
         <span style={style}> | </span>
--- src/constants/types.js
+++ src/constants/types.js
@@ -1,7 +1,9 @@
 
 export const NODE_TYPE_UNDEFINED = "undefined";
 export const NODE_TYPE_TEXT = "text";
-export const NODE_TYPE_IMAGE = "image";
+export const NODE_TYPE_FILE = "file";
+export const NODE_TYPE_TODO = "todo";
+
 export const TYPE_NODE = "node";
 export const TYPE_LINE = "line";
 export const TYPE_NONE = "none";
