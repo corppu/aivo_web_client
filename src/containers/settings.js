e56c8b0b74c394a625907306d0e995ae074f43d7
Pasi Hyvönen <Pasi Hyvnen>
2017-05-07 17:20:47 +0300

    Add settings component stump

---
+++ src/containers/settings.js
@@ -0,0 +1,9 @@
+import { connect } from "react-redux";
+
+import Settings from "../components/settings";
+
+function mapStateToProps(state) {
+    return {};
+}
+
+export default connect(mapStateToProps)(Settings);
