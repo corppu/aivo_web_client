5244592a3d2a9baf2d6aa57a3f4c73221440f37a
Pasi Hyvönen <Pasi Hyvnen>
2017-05-21 12:02:46 +0300

    Add loading/error state to file node view

--- src/components/node-view-file.js
+++ src/components/node-view-file.js
@@ -1,51 +1,84 @@
-import React from "react";
+import React, { createClass } from "react";
 
-export default function({ imgURL, updateState }) {
+const NodeViewFile = createClass({
+    getInitialState: function() {
+        return {
+            isImageLoading: false,
+            imageError: null
+        }
+    },
 
-    // TODO: Handle excessively tall media properly
+    render: function() {
+        const { imgURL, updateState } = this.props;
 
-    return (
-        <div>
-            <center
-                style={{
-                    marginBottom: 24
-                }}>
-                { imgURL && imgURL.length > 0
-                    ? <img
-                        style={{
-                            display: "block",
-                            maxWidth: "100%"
-                        }}
-                        src={imgURL}
-                        //onLoad={e => { console.log(e); }}
-                        //onError={e => { console.log(e); }}
-                        />
-                    : <div
-                        style={{
-                            color: "#e4e4e4"
-                        }}>
-                        <div
+        // TODO: Handle excessively tall media properly
+
+        return (
+            <div>
+                <center
+                    style={{
+                        marginBottom: 24
+                    }}>
+                    { imgURL && imgURL.length > 0
+                        ? <img
                             style={{
-                                padding: "72px 0",
-                                border: "dashed",
-                                borderWidth: 2,
-                                borderColor: "#e4e4e4"
+                                display: "block",
+                                maxWidth: "100%"
+                            }}
+                            src={imgURL}
+                            onLoad={this.onImageLoad}
+                            onError={this.onImageError}
+                            />
+                        : <div
+                            style={{
+                                color: "#e4e4e4"
                             }}>
-                            <i className="fa fa-picture-o fa-5x"/>
+                            <div
+                                style={{
+                                    padding: "72px 0",
+                                    border: "dashed",
+                                    borderWidth: 2,
+                                    borderColor: "#e4e4e4"
+                                }}>
+                                <i className="fa fa-picture-o fa-5x"/>
+                            </div>
                         </div>
-                    </div>
-                }
-            </center>
-            <input
-                className="node-input input width-full"
-                value={ imgURL || "" }
-                placeholder="Insert file URL here..."
-                onChange={(e) => {
-                    const { value } = e.target;
-                    
+                    }
+                </center>
+                <input
+                    className="node-input input width-full"
+                    value={ imgURL || "" }
+                    placeholder="Insert file URL here..."
+                    onChange={(e) => {
+                        const { imgURL } = this.props;
+                        const { value } = e.target;
+                        
+                        updateState({ imgURL: value });
 
-                    updateState({ imgURL: value });
-                }}/>
-        </div>
-    );
-}
+                        if (value !== imgURL) {
+                            this.setState({
+                                isImageLoading: true,
+                                imageError: null
+                            });
+                        }
+                    }}/>
+            </div>
+        );
+    },
+
+    onImageLoad: function(e) {
+         this.setState({
+            isImageLoading: false,
+            imageError: null
+        });
+    },
+
+    onImageError: function(e) {
+         this.setState({
+            isImageLoading: false,
+            imageError: e
+        });
+    }
+});
+
+export default NodeViewFile;
