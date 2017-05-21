7698791d056d82b5db4e8ba6c2885b83d7a3d3ea
Pasi Hyvönen <Pasi Hyvnen>
2017-05-21 17:02:44 +0300

    Implement settings base layout

--- src/components/settings.js
+++ src/components/settings.js
@@ -3,8 +3,32 @@ import React, { createClass } from "react";
 const Settings = createClass({
     render: function() {
         return (
-            <div>
-                SETTINGS PLACEHOLDER
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center"
+                }}>
+                <div
+                    style={{
+                        width: 800,
+                        alignContent: "left",
+                        fontSize: "36px",
+                        color: "#787c96"
+                    }}>
+                    Settings
+                </div>
+                <div
+                    style={{
+                        width: 800,
+                        height: 854,
+                        backgroundColor: "#ffffff",
+                        border: "solid 1px #c5c5c5"
+                    }}>
+
+                    PLACEHOLDER
+
+                </div>
             </div>
         );
     }
